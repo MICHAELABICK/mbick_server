e23320cdf7e1f6d5d71eab01fb397e1fd171b43a
MICHAELABICK <MichaelBick@Michaels-MacBook-Pro-7.local>
2019-09-15 12:57:33 -0400

    Adjust dynamic inventory wrapper script to match vendored script

--- provisioning/inventory/proxmox.bash
+++ provisioning/inventory/proxmox.bash
@@ -5,9 +5,7 @@ set -e
 # change directory to location of this script
 SRC_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null && pwd )"
 PROJECT_DIR="$(cd "${SRC_DIR}/../.."; pwd)"
-# SCRIPT_DIR="$PROJECT_DIR/vendor/ansible-proxmox-inventory"
-# SCRIPT_DIR="$PROJECT_DIR/vendor/github.com/RaSerge/ansible-proxmox-inventory"
-SCRIPT_DIR="$PROJECT_DIR/../Ansible-Proxmox-inventory"
+SCRIPT_DIR="$PROJECT_DIR/vendor/github.com/MICHAELABICK/Ansible-Proxmox-inventory"
 
 PROXMOX_URL=$(/bin/bash "$PROJECT_DIR/scripts/parse_env_file" \
     "$PROJECT_DIR/common.env" PROXMOX_URL)"/"
@@ -20,6 +18,6 @@ python "$SCRIPT_DIR/proxmox.py" \
     --url="$PROXMOX_URL" \
     --username="$PROXMOX_USERNAME" \
     --password="$PROXMOX_PASSWORD" \
-    --qemu_default_interface \
+    --qemu-default-interface \
     --trust-invalid-certs \
     --list --pretty
