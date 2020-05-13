0e0eaef1a1159e795a0785dd0b1ecc1d0c83b928
Michael Bick <mafbick@gmail.com>
2020-05-12 23:13:37 -0400

    Rewrote proxmox.bash inventory script to use Dhall config and Vault secrets

--- mbick_server/src/dhall-packages/mbick-server/ansible.dhall
+++ mbick_server/src/dhall-packages/mbick-server/ansible.dhall
@@ -4,6 +4,14 @@ let HostURL/show = networking.HostURL.show
 let config = ./config.dhall
 
 in {
+, vault_api_url =
+    HostURL/show
+    config.vault_api.address
+, proxmox_api_url =
+    HostURL/show
+    ( config.proxmox_api.address
+      // { endpoint = None Text }
+    )
 , ssh_user_ca_public_key_endpoint =
     HostURL/show
     ( config.vault_api.address
--- mbick_server/src/provisioning/inventory/proxmox.bash
+++ mbick_server/src/provisioning/inventory/proxmox.bash
@@ -8,15 +8,16 @@ PROJECT_DIR="$(cd "${SRC_DIR}/../.."; pwd)"
 VENDOR_DIR="$(cd "${PROJECT_DIR}/../../vendor"; pwd)"
 SCRIPT_DIR="$VENDOR_DIR/github.com/MICHAELABICK/Ansible-Proxmox-inventory"
 
-PROXMOX_URL=$(/bin/bash "$PROJECT_DIR/scripts/parse_env_file" \
-    "$PROJECT_DIR/common.env" PROXMOX_URL)"/"
-PROXMOX_USERNAME=$(/bin/bash "$PROJECT_DIR/scripts/parse_env_file" \
-    "$PROJECT_DIR/defaults.env" PROXMOX_USER)
-PROXMOX_PASSWORD=$(/bin/bash "$PROJECT_DIR/scripts/parse_env_file" \
-    "$PROJECT_DIR/defaults.env" PROXMOX_PASSWORD)
+eval $(dhall-to-bash --declare VAULT_API_URL <<< \
+  "let packages = ${PROJECT_DIR}/packages.dhall in packages.mbick-server.ansible.vault_api_url")
+eval $(dhall-to-bash --declare PROXMOX_API_URL <<< \
+  "let packages = ${PROJECT_DIR}/packages.dhall in packages.mbick-server.ansible.proxmox_api_url")
+
+PROXMOX_USERNAME=$(vault kv get -address=$VAULT_API_URL -field=username proxmox_user/ansible-inventory)
+PROXMOX_PASSWORD=$(vault kv get -address=$VAULT_API_URL -field=password proxmox_user/ansible-inventory)
 
 python "$SCRIPT_DIR/proxmox.py" \
-    --url="$PROXMOX_URL" \
+    --url="$PROXMOX_API_URL" \
     --username="$PROXMOX_USERNAME" \
     --password="$PROXMOX_PASSWORD" \
     --qemu-default-interface \
